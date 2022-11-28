daae2fbc10ece81ae2a39a9f59a402ca03508df8
critical85 <59550613+critical85@users.noreply.github.com>
2022-11-28 02:58:15 +0100

    removed unused imports

--- Astronauts.Core/Repositories/IRepository.cs
+++ Astronauts.Core/Repositories/IRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Astronauts.Core.Repositories
+﻿namespace Astronauts.Core.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
